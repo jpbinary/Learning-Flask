e522f375bb24f35e2287f1222bb6b6444d77f77e
jpbinary <jp@iskyy.com>
2015-10-25 11:22:33 -0500

    added routes

--- sched/app.py
+++ sched/app.py
@@ -10,6 +10,27 @@ app = Flask(__name__)
 def hello():
     return 'Hello world!'
 
+@app.route('/appointments/')
+def appointment_list():
+    return 'Listing of all appointments we have.'
+
+@app.route('/appointments/<int:appointment_id>/')
+def appointment_detail(appointment_id):
+    return 'Detail of appointment #{}.'.format(appointment_id)
+
+@app.route('/appointments/<int:appointment_id>/edit/', methods=['GET', 'POST'])
+def appointment_edit(appointment_id):
+    return 'Form to edit appointment #{}'.format(appointment_id)
+
+@app.route('/appointments/create/', methods=['GET', 'POST'])
+def appointment_create():
+    return 'Form to create a new appointment.'
+
+@app.route('/appointments/<int:appointment_id>/delete/', methods=['DELETE'])
+def appointment_delete(appointment_id):
+    raise NotImplementedError('DELETE')
+
+
 if __name__ == '__main__':
     pass
     # Run the python dev web server, but only if this file called directly (not via import)
