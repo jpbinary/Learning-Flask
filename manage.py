9f18d1063e931d33e152a7090b565e19aff461fb
jpbinary <jp@iskyy.com>
2015-10-25 10:40:44 -0500

    added Flask-Script support

--- manage.py
+++ manage.py
@@ -1,1 +1,14 @@
+# Manage application with Flask-Script
+#   which gives you the runserver and shell commands from the command line
+from flask.ext.script import Manager
+from sched.app import app
 
+manager = Manager(app)
+app.config['DEBUG'] = True # To turn on debugging
+
+if __name__ == '__main__':
+    manager.run()
+
+# run app from the command line:
+#   $ python manage.py runserver
+#   $ python manage.py runserver -h 0.0.0.0 -p 8000
--- sched/app.py
+++ sched/app.py
@@ -1,1 +1,26 @@
+from flask import Flask
 
+# Create a python object named app that is a WSGI application.
+#   __name__ argument tells Flask to look in the current module (file) for resources
+app = Flask(__name__)
+
+# Set up function to handle requests to URL '/' (aka View functions)
+#   View functions return strings when requests made
+@app.route('/')
+def hello():
+    return 'Hello world!'
+
+if __name__ == '__main__':
+    pass
+    # Run the python dev web server, but only if this file called directly (not via import)
+    #app.run()
+    # When run, should see: "Running on http://127.0.0.1:5000/"
+
+    # If need app to be accessible on network, replace 'app.run()' with:
+    #   app.run('0.0.0.0') or app.run('0.0.0.0', 8000) to listen on port 8000, etc...
+    #app.run(debug=True)
+    # Do not use debug mode on live server!
+
+    # Alternatively, you can use Flask-Script with a manage.py file to manage
+    #   how you want to run the app. This way you don't need to enter
+    #   app.run() within this program file.
